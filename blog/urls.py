6c279514d722f282ac03a4400a91076fe631f581
Daniel Istrate <16302386+istrate@users.noreply.github.com>
2022-04-01 21:57:27 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^login/$', auth_views.login, {'template_name': 'blog/login.html'}, name='login'),
+    url(r'^login/$', auth_views.LoginView, {'template_name': 'blog/login.html'}, name='login'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
